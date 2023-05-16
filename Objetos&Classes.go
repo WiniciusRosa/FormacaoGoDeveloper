6bb4214194c47e4567139dd14fade370e8265b7a
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-15 22:43:07 -0300

    Create Objetos&Classes.go

---
+++ Objetos&Classes.go
@@ -0,0 +1,10 @@
+type Pessoa struct {
+	Nome      string
+	Sobrenome string
+	Idade     int8
+
+	// pessoa1 := Pessoa{"Winicius","Rosa","22"}
+	// pessoa2 := pessoa{Nome: "Winicius", Sobrenome: "Rosa", Idade: 22}
+	// pessoa3 := Pessoa{Nome: "Winicius", Idade: 22}
+
+}
