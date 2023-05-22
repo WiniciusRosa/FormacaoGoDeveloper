edf3eff19ca7d3d2825021a4c6c90858e0e25e18
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-22 15:23:29 -0400

    Create PrintLnXPrintF.go

---
+++ PrintLnXPrintF.go
@@ -0,0 +1,17 @@
+
+package main
+
+import "fmt"
+
+
+ Const ebulicaoF  float64 = 212.0
+
+   func main() {
+ 
+	var tempF = ebulicaoF // Variável do valor temperatura em graus F
+	Var tempC = (tempF - 32) * 5 / 9  // Conversão de F para C
+
+	fmt.PrintF("A temperatua de ebulição da água em ºF é de    %g           e A temperatua de ebulição da água em ºC é de  %g        ", tempF, tempC)
+       
+	
+}
