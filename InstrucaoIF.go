0ef65b446c744ea4b9703d115692ce8b2fdec319
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-22 16:54:07 -0400

    Create InstrucaoIF.go

---
+++ InstrucaoIF.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	////Tipo 3
+
+	for i := 1; i <= 10; i++ {
+
+		if i%2 == 0 {
+			fmt.Println("Par,", i)
+		} else {
+			fmt.Println("Ímpar,", i)
+		}
+	}
+
+	// se o Numero é par ou ímpar
+	// resto %
+
+}
