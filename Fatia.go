6d7e20a9a6e2ac99082a880920e21d00c619c244
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-23 14:06:59 -0400

    Create Fatia.go

---
+++ Fatia.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fatia1 := []int(1)
+	fatia2 := append(fatia1, 4, 5)
+	fmt.Println(fatia1, fatia2)
+
+	//	arr := [7]float64(1, 2, 3, 4, 5, 6, 7)
+	//	fatia := arr[0:7]
+	//	fmt.Println(fatia)
+
+	//	fatia := make([]float64, 5)
+	//	fmt.Println(fatia)
+
+	// var x []float64
+	//fatia := make([]float64,)
+	//fatia := arr{0:5]
+	//acrescentar elemento APPEND
+}
