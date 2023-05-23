282590977be4aaa1b6a4bde73de5f869bf57a3a9
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-23 09:38:31 -0400

    Create InstrucaoSwitch.go

---
+++ InstrucaoSwitch.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <= 10; i++ {
+
+		switch i {
+
+		case 0:
+			fmt.Println("Zero")
+		case 1:
+			fmt.Println("Um")
+		case 2:
+			fmt.Println("Dois")
+		case 3:
+			fmt.Println("Tres")
+		case 4:
+			fmt.Println("Quatro")
+		case 5:
+			fmt.Println("Cinco")
+		}
+
+	}
+
+	//	for i := 0; i <= 10; i++ {
+	//
+	//		if i == 0 {
+	//			fmt.Println("Zero")
+	//		} else if i == 1 {
+	//			fmt.Println("Um")
+	//		} else if i == 2 {
+	//			fmt.Println("Dois")
+	//		} else if i == 3 {
+	//			fmt.Println("Tres")
+	//		}
+	//	}
+
+}
