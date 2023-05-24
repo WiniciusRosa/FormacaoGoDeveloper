720f7c3f5486b915645a2e7e557e261487d321dd
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-24 16:33:50 -0400

    Create PacotePathFilepath.go

---
+++ PacotePathFilepath.go
@@ -0,0 +1,17 @@
+// Implementa rotinas de utilitáriopara manipular caminhos de nome de arquivo
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	})
+
+}
