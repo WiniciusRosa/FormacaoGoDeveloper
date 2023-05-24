0575b2937eb24deca021be9b3ba599d3f751e3be
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-24 16:11:03 -0400

    Create PacoteOS.go

---
+++ PacoteOS.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+
+	f, err := os.OpenFile("notes.text", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
