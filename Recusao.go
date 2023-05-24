544aad75c6bc2c8016285ffa8ead6e400ba1a796
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-24 13:22:04 -0400

    Create Recusao.go

---
+++ Recusao.go
@@ -0,0 +1,20 @@
+// Recursão é a capcidade de uma função chamar ela mesma
+// Calcular fatorial
+
+package main
+
+import "fmt"
+
+func fatorial(x int) int {
+
+	if x == 0 {
+		return 1
+	}
+	return x * fatorial(x-1)
+}
+
+func main() {
+
+	fmt.Println(fatorial(4))
+
+}
