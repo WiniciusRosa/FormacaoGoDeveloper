4ed767f1bd7e954bcca8791691d4dc5c82d1f3e5
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-25 14:13:46 -0400

    Create ArquivoJSON.go

---
+++ JsonGO/ArquivoJSON.go
@@ -0,0 +1,26 @@
+{
+	
+	"usuarios": [
+	{ "Nome"  : "Winicius",
+	  "Tipo"  : "Analista de Sistema",
+	  "Idade" : 22,
+	  
+	  "Social" : {
+		   "Facebook" : "http://facebook.com.br/Winicis-Rosa"
+		   "Twitter"  : "http://Twitter.com.br/Winicis-Rosa"
+	            }
+		
+	  
+	},
+	
+	{"Nome"  : "Winicius",
+	  "Tipo"  : "Analista de Sistema",
+	  "Idade" : 22,
+	  
+	  "Social" : {
+		   "Facebook" : "http://facebook.com.br/Winicis-Rosa"
+		   "Twitter"  : "http://Twitter.com.br/Winicis-Rosa"
+	  }
+	} 
+  ]
+}
