9ca1cb019655ed1281c3a81522b3b19ee8e3ee73
Winicius Rosa <97139599+WiniciusRosa@users.noreply.github.com>
2023-05-26 10:25:07 -0400

    Update MeuPingPong.go

--- DesafiosGO/PingPong.go
+++ DesafiosGO/PingPong.go
@@ -1,1 +1,20 @@
-Fazendo...
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 20; i++ {
+		if i%3 == 0 && i%4 == 0 {
+			fmt.Println("BateuVoltou")
+		} else if i%3 == 0 {
+			fmt.Println("Bateu")
+		} else if i%5 == 0 {
+			fmt.Println("Voltou")
+		} else {
+			fmt.Println(i)
+
+		}
+
+	}
+}
